266679e21196c8a893ebdfe6bca9802c9485bf01
Cam Pedersen <diffference@gmail.com>
2013-06-04 20:32:28 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var ejs = require('ejs');
+
+var views = {};
+var path = 'views/';
+
+module.exports = function (req, res) {
+  res.render = function (view, options) {
+    options = options || {};
+    var layout = options.layout || 'layout';
+
+    loadView(view, function (bodyStr) {
+      var body = ejs.render(bodyStr, options);
+      options.body = body;
+
+      loadView(layout, function (layoutStr) {
+        var ret = ejs.render(layoutStr, options);
+        res.end(ret);
+      });
+    });
+  }
+}
+
+function loadView (name, callback) {
+  if (views[name]) {
+    callback(views[name]);
+    return;
+  } else {
+    fs.readFile(path + name + '.ejs', function (err, data) {
+      if (err) throw err;
+      data = data.toString();
+      views[name] = data;
+      callback(data);
+    });
+    return;
+  }
+}
